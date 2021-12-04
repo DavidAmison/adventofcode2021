41b8e678d63c2c628332654e84afec61bebe58ae
davidamison <david.amison20@gmail.com>
2021-12-04 11:11:45 +0000

    Skeletens for all days

---
+++ day10/src/main.rs
@@ -0,0 +1,15 @@
+use utils::{files, parse_field};
+
+fn main() {
+    let filename = "src/input.txt";
+    let matrix = files::read_in_matrix_as::<u32>(filename);
+
+    println!("----- PART 1 -----");
+
+    println!("Part 1 Answer: {}", 0);
+
+    println!("\n\n----- PART 2 -----");
+
+    println!("Part 2 Answer: {}", 0);
+
+}
