f4d54b6d5d6b2df1427960bc64eb1e48ad6ed7f5
davidamison <david.amison20@gmail.com>
2021-12-04 11:12:35 +0000

    Additional file utils *  Read in csv *  Read in matrix chunks

--- utils/src/files.rs
+++ utils/src/files.rs
@@ -2,6 +2,48 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 
+/// Read in separated values of line of a file to a vector
+///
+/// # Arguments
+///
+/// * `filename` - String containing the filename
+/// * `separator` - Separator for the values
+pub fn read_in_sv(filename: &str, separator: &str) -> Vec<String> {
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+    reader.read_line(&mut line).ok();
+
+    line.split(separator)
+        .map(|s| String::from(s))
+        .collect()
+}
+
+/// Read in separated values of line of a file to a vector
+///
+/// # Arguments
+///
+/// * `filename` - String containing the filename
+/// * `separator` - Separator for the values
+pub fn read_in_sv_as<T>(filename: &str, separator: &str) -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+    reader.read_line(&mut line).ok();
+
+    line.split(separator)
+        .map(|s| s.parse::<T>().unwrap())
+        .collect()
+}
+
 /// Read in lines of a file to a vector
 ///
 /// # Arguments
@@ -133,6 +175,47 @@ pub fn read_in_chunks(filename: &str) -> Vec<Vec<String>> {
             chunk = Vec::new();
         } else {
             chunk.push(line);
+        }
+    }
+    chunks
+}
+
+/// Lists of values separated by empty lines
+pub fn read_in_matrix_chunks(filename: &str) -> Vec<Vec<Vec<String>>> {
+    let mut chunks = Vec::new();
+    let mut chunk = Vec::new();
+    for line in read_in_lines(filename) {
+        if line.trim().is_empty() {
+            chunks.push(chunk.clone());
+            chunk = Vec::new();
+        } else {
+            let row = line.split_whitespace()
+                .map(|s| String::from(s))
+                .collect();
+            chunk.push(row);
+        }
+    }
+    chunks
+}
+
+/// Lists of values separated by empty lines
+pub fn read_in_matrix_chunks_as<T>(filename: &str) -> Vec<Vec<Vec<T>>>
+where
+    T: std::str::FromStr,
+    T: Clone,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut chunks = Vec::new();
+    let mut chunk = Vec::new();
+    for line in read_in_lines(filename) {
+        if line.trim().is_empty() {
+            chunks.push(chunk.clone());
+            chunk = Vec::new();
+        } else {
+            let row = line.split_whitespace()
+                .map(|s| s.parse::<T>().unwrap())
+                .collect();
+            chunk.push(row);
         }
     }
     chunks
