8a895c18fa71c7a3b4abe14cd0cf38035cdf57b3
davidamison <david.amison20@gmail.com>
2021-12-07 21:13:11 +0000

    day07 initial solution

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,15 +1,57 @@
 use utils::{files, parse_field};
 
 fn main() {
-    let filename = "src/input.txt";
-    let matrix = files::read_in_matrix_as::<u32>(filename);
+    let filename = "src/test_input.txt";
+    let positions = files::read_in_sv_as::<i32>(filename, ",");
 
     println!("----- PART 1 -----");
+    let mut minimum = positions.iter().sum();
 
-    println!("Part 1 Answer: {}", 0);
+    let consumption = |x: i32, position: i32| {
+        (x - position).abs()
+    };
+
+    let mut position = 0;
+    loop {
+        position += 1;
+        let fuel: i32 = positions.iter()
+            .map(|x| consumption(*x, position))
+            .sum();
+        // println!("Offset {} - Fuel {}", offset, fuel);
+        if fuel < minimum {
+            minimum = fuel
+        } else {
+            break
+        };
+    }
+    println!("Part 1 Answer: {}", minimum);
 
     println!("\n\n----- PART 2 -----");
+    let mut minimum = positions.iter()
+        .map(|x| triangle(*x))
+        .sum();
 
-    println!("Part 2 Answer: {}", 0);
+    let triangle_consumption = |x: i32, position: i32| {
+        triangle((x - position).abs())
+    };
 
+    let mut position = 0;
+    loop {
+        position += 1;
+        let fuel: i32 = positions.iter()
+            .map(|x| triangle_consumption(*x, position))
+            .sum();
+        // println!("Offset {} - Fuel {}", offset, fuel);
+        if fuel < minimum {
+            minimum = fuel
+        } else {
+            break
+        };
+    }
+    println!("Part 2 Answer: {}", minimum);
+
+}
+
+fn triangle(x: i32) -> i32 {
+    x * (x+1) / 2
 }
