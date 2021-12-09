267d3d2466d7532ffe5e198863435546aed76a56
davidamison <david.amison20@gmail.com>
2021-12-09 13:02:10 +0000

    Added parse_field_unwrap macro

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -21,6 +21,27 @@ macro_rules! parse_field {
     };
 }
 
+#[macro_export]
+macro_rules! parse_field_unwrap {
+    ($input_string:expr => $($type:ty, $seperator:literal) |+) => {
+        {
+            let mut right: &str = $input_string;
+            ( $(
+                if $seperator == "" {
+                    right.parse::<$type>().ok().unwrap()
+                } else {
+                    if let Some((l, r)) = right.split_once($seperator) {
+                        right = r;
+                        l.parse::<$type>().ok().unwrap()
+                    } else {
+                        None.unwrap()
+                    }
+                },
+            )* )
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
