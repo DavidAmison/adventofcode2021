4de0e6ce78fa6bb59de88213b86624b308f05a8d
davidamison <david.amison20@gmail.com>
2021-12-23 12:16:50 +0000

    Day 22 full solution

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,6 +1,9 @@
 use utils::{files, parse_field_unwrap};
 use std::collections::HashMap;
 
+// mod cube;
+use cube::*;
+
 fn main() {
     let filename = "input";
     let lines = files::read_in_lines(filename);
@@ -13,97 +16,98 @@ fn main() {
         };
         RebootStep {
             instr,
-            x: (i.1, i.2),
-            y: (i.3, i.4),
-            z: (i.5, i.6),
+            x: (i.1, i.2+1),
+            y: (i.3, i.4+1),
+            z: (i.5, i.6+1),
         }
     }).collect();
 
+    // Add cubes to a list with following rules:
+    // - Check for all intersections with previous cubes and 'nullify' that intersection
+    //   by adding a negative weighted cube
+    // - If we have an ON instruction then add the cube in the instruction
+
+    // Weighted cubes (+1 or -1)
+    let mut cubes: Vec<(Cube, isize)> = Vec::new();
+    for step in steps.iter() {
+        let cube = Cube::new(step.x, step.y, step.z);
+        let mut update: Vec<(Cube, isize)> = Vec::new();
+        for (c, i) in cubes.iter() {
+            if let Some(int) = cube.intersection(&c) {
+                update.push((int, -1 * i));  // Insert opposite (i.e. nullify cube)
+            }
+        }
+        match step.instr {
+            Instruction::On => update.push((cube, 1)),
+            Instruction::Off => (), // Do nothing
+        };
+
+        cubes.append(&mut update);
+    }
 
     println!("----- PART 1 -----");
-    let mut points = HashMap::new();
-    for step in steps {
-        execute_step(step, &mut points);
-    }
 
-    // Count points in 50x50x50 cube
-    let mut count = 0;
-    for (point, state) in points {
-        count += match state {
-            State::On => {
-                if point_in_range(point, (-50, 50)) {
-                    1
-                } else {
-                    0
-                }
-            }
-            _ => 0
-        }
-    }
-    println!("Part 1 Answer: {}", count);
+    let count_area = Cube::new((-50, 51), (-50, 51), (-50, 51));
+    let volume = cubes.iter().map(|(c, w)| (c.intersection(&count_area), w))   // Restrict to area of interest
+        .filter(|(c, _)| c.is_some())                                          // Filter out non-intersecting cubes
+        .fold(0, |vol, (c, w)| vol + w * c.unwrap().volume());                 // Calculate weighted volume
+
+    println!("Part 1 Answer: {}", volume);
 
     println!("\n\n----- PART 2 -----");
 
-    println!("Part 2 Answer: {}", 0);
+    let volume = cubes.iter().fold(0, |vol, (c, w)| vol + w * c.volume());
 
+    println!("Part 2 Answer: {}", volume);
 }
 
-fn execute_step(s: RebootStep, points: &mut HashMap<XYZ, State>) {
-    for x in s.x.0..=s.x.1 {
-        if x < -50 || x > 50 {
-            continue;
-        }
-        for y in s.y.0..=s.y.1 {
-            if y < -50 || y > 50 {
-                continue;
+mod cube {
+    pub type Range = (isize, isize);
+
+    #[derive(Debug)]
+    pub struct Cube {
+        x: Range,
+        y: Range,
+        z: Range,
+    }
+
+    impl Cube {
+        pub fn new(x: Range, y: Range, z: Range) -> Self {
+            Cube {
+                x,
+                y,
+                z,
             }
-            for z in s.z.0..=s.z.1 {
-                if z < -50 || z > 50 {
-                    continue;
-                }
-                let p = points.entry((x, y, z)).or_insert(State::Off);
-                match s.instr {
-                    Instruction::On => *p = State::On,
-                    Instruction::Off => *p = State::Off,
-                }
+        }
+
+        pub fn volume(&self) -> isize {
+            (self.x.1 - self.x.0) * (self.y.1 - self.y.0) * (self.z.1 - self.z.0)
+        }
+
+        pub fn intersection(&self, other: &Self) -> Option<Cube> {
+            let x = (std::cmp::max(self.x.0, other.x.0), std::cmp::min(self.x.1, other.x.1));
+            let y = (std::cmp::max(self.y.0, other.y.0), std::cmp::min(self.y.1, other.y.1));
+            let z = (std::cmp::max(self.z.0, other.z.0), std::cmp::min(self.z.1, other.z.1));
+            if x.0 < x.1 && y.0 < y.1 && z.0 < z.1 {
+                Some(Cube{x, y, z})
+            } else {
+                None
             }
         }
     }
+
+    #[derive(Debug)]
+    pub struct RebootStep {
+        pub instr: Instruction,
+        pub x: Range,
+        pub y: Range,
+        pub z: Range,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum Instruction {
+        On,
+        Off,
+    }
 }
 
-fn point_in_range(p: XYZ, r: Range) -> bool {
-    p.0 >= r.0 && p.0 <= r.1 &&
-    p.1 >= r.0 && p.1 <= r.1 &&
-    p.2 >= r.0 && p.2 <= r.1
-}
-
-fn clamp_range(r: Range, limits: Range) -> Range {
-    (
-        if r.0 < limits.0 { limits.0 } else if r.0 > limits.1 { limits.1 } else { r.0 },
-        if r.1 < limits.0 { limits.0 } else if r.1 > limits.1 { limits.1 } else { r.1 },
-    )
-}
-
-type XYZ = (isize, isize, isize);
-
-type Range = (isize, isize);
-
-#[derive(Debug)]
-struct RebootStep {
-    instr: Instruction,
-    x: Range,
-    y: Range,
-    z: Range,
-}
-
-#[derive(Debug, PartialEq)]
-enum Instruction {
-    On,
-    Off,
-}
-
-#[derive(Debug, PartialEq)]
-enum State {
-    On,
-    Off,
-}
